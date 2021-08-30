1997152b7a518a9265d4a84656aa999dffe22185
Vitor Hugo Pereira Silva <vitor_hugo_ps@hotmail.com>
2021-08-30 11:59:51 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/cursoflutter/px/sf_mini_soundboard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cursoflutter.px.sf_mini_soundboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:sf_mini_soundboard/Home.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  darkTheme: ThemeData.dark(),
+  home: Home(),
+));
